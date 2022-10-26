b7db9b33cd186d22b68893c0ce77499a3b120293
Jarvis0p <krishnajaiswal.2208@gmail.com>
2022-10-26 14:08:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+var count = 1;
+var dutyIncIndex = 1;
+
+function check() {
+    var date = new Date();
+    var hr = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    console.log(hr, min, sec);
+    console.log("Being Called");
+    if (hr === 11 && min === 51 && sec === 0) {
+        count = count + 1;
+        console.log("count from " + count);
+        console.log("Count " + count);
+        dutyIncIndex = count % 3;
+        console.log("true");
+    }
+}
+
+setInterval(check, 1000)
+app.get("/", function (req, res) {
+    res.render("home", {
+        data: dutyIncIndex
+    })
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server Started on port 3000");
+});
